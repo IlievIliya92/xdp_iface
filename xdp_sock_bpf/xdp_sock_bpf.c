45f11cd19ddeaa69f3f5cd8ad747c90baae79db7
Iliya Iliev <iliyailiev3592@gmail.com>
2023-04-16 16:45:44 +0300

    xdp_sock_bpf: Add xsks map & redirect pkts
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

--- xdp_sock_bpf/xdp_sock_bpf.c
+++ xdp_sock_bpf/xdp_sock_bpf.c
@@ -2,10 +2,23 @@
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
 
+#define MAX_SOCKS 4
+
+struct {
+    __uint(type, BPF_MAP_TYPE_XSKMAP);
+    __uint(max_entries, MAX_SOCKS);
+    __uint(key_size, sizeof(int));
+    __uint(value_size, sizeof(int));
+} xsks_map SEC(".maps");
+
+static unsigned int rr;
+
 SEC("xdp_sock")
 int xdp_sock_prog(struct xdp_md *ctx)
 {
-    return XDP_DROP;
+    rr = (rr + 1) & (MAX_SOCKS - 1);
+
+    return bpf_redirect_map(&xsks_map, rr, XDP_DROP);
 }
 
 char _license[] SEC("license") = "GPL";
