baa29ee17f7d76d547e3a2ea9ecc11bdde34cbe6
Iliya Iliev <iliyailiev3592@gmail.com>
2023-04-23 15:36:38 +0300

    bindings: go: Add go bindings
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

---
+++ bindings/go/xdpiface/xdpiface_test.go
@@ -0,0 +1,61 @@
+package xdpiface
+
+import (
+	"testing"
+	"encoding/hex"
+	"bytes"
+	"fmt"
+)
+
+func TestXdpIface(t *testing.T) {
+	xdp_iface, err := XdpIfaceNew("lo")
+	if err != nil {
+		t.Errorf("Failed to create XDP iface")
+	}
+	defer xdp_iface.Destroy()
+}
+
+func TestXdpSock(t *testing.T) {
+	batch_size := uint32(30)
+	var frames_rcvd uint32 = 0
+
+	var oBufferSize int = 1000
+	oBuffer := bytes.Repeat([]byte{0x55}, oBufferSize)
+
+	var iBufferSize int = 0
+	iBuffer := make([]byte, 9000)
+
+	xdp_iface, err := XdpIfaceNew("lo")
+	if err != nil {
+		t.Errorf("Failed to create XDP iface")
+	}
+	defer xdp_iface.Destroy()
+
+	xdp_iface.LoadProgram("./../../../build/xdp_sock_bpf.o")
+
+	xdp_sock, err := XdpSockNew(xdp_iface)
+	if err != nil {
+		t.Errorf("Failed to create XDP sock")
+	}
+	defer xdp_sock.Destroy()
+	xdp_sock.LoopUpBpfMap(xdp_iface, "xsks_map", 4, 4)
+
+    xdp_sock.SetSockopt(SO_PREFER_BUSY_POLL, 1)
+    xdp_sock.SetSockopt(SO_BUSY_POLL, 20)
+    xdp_sock.SetSockopt(SO_BUSY_POLL_BUDGET, int(batch_size))
+
+    xdp_sock.TxBatchSetSize(batch_size);
+  	for i := 1; i <= int(batch_size); i++ {
+        xdp_sock.Send (oBuffer, oBufferSize)
+	}
+    xdp_sock.TxBatchRelease(batch_size);
+
+    xdp_sock.RxBatchGetSize (&frames_rcvd, batch_size);
+	for i := 1; i <= int(frames_rcvd); i ++ {
+        xdp_sock.Recv (iBuffer, &iBufferSize)
+		fmt.Println(hex.Dump(iBuffer[:iBufferSize]))
+    }
+    xdp_sock.RxBatchRelease(frames_rcvd)
+
+	xdp_iface.UnloadProgram()
+}
