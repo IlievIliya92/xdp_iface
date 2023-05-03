bb134a9ad61a4238951e5eba3d01e90ed2672e49
Iliya Iliev <iliyailiev3592@gmail.com>
2023-05-04 01:38:11 +0300

    builds: vm: fedora: Add XDP iface selftest
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

---
+++ builds/vm/fedora/xdp_iface_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+# Test script
+
+IP="192.168.122.2"
+
+wait-for-it "$IP:22" -t 300 -s -- echo ready
+
+set -x
+sshpass -p fedora ssh -o "StrictHostKeyChecking=no" "root@$IP" uname -a
+sshpass -p fedora ssh "root@$IP" "/home/xdp_iface_work_dir/xdp_iface/build/xdpiface_selftest"
+
+sudo virsh shutdown f37vm
+until sudo virsh domstate f37vm | grep shut; do
+    sleep 10
+done
