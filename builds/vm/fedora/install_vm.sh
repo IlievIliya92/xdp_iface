569d5710543949dfbd3336fbc5ed81207697c557
Iliya Iliev <iliyailiev3592@gmail.com>
2023-05-04 11:01:52 +0300

    builds: vm: fedora: Specify os variant & disable autoconsole
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

--- builds/vm/fedora/install_vm.sh
+++ builds/vm/fedora/install_vm.sh
@@ -2,5 +2,8 @@
 
 virt-install --name f37vm --ram 4096 \
     --disk path=f37vm.qcow2,format=qcow2,cache=writeback \
-    --nographics --os-variant detect=on --import \
-    --network bridge=virbr0
+    --nographics --os-variant fedora35 --import \
+    --network bridge=virbr0 \
+    --noautoconsole
+
+virsh list
