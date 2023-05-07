9e428cc7f9013abbce040dae3f34189515855f80
Iliya Iliev <iliyailiev3592@gmail.com>
2023-05-07 19:37:12 +0300

    builds: vm: fedora: Add vm ip to known_hosts
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

--- builds/vm/fedora/xdp_iface_test.sh
+++ builds/vm/fedora/xdp_iface_test.sh
@@ -6,6 +6,7 @@ IP="192.168.122.2"
 
 wait-for-it "$IP:22" -t 900 -s -- echo ready
 
+sudo ssh-keygen -f "/root/.ssh/known_hosts" -R "$IP"
 set -x
 sshpass -p fedora ssh -o "StrictHostKeyChecking=no" "root@$IP" uname -a
 sshpass -p fedora ssh "root@$IP" "/home/xdp_iface_work_dir/xdp_iface/build/xdpiface_selftest"
