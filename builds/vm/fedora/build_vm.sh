491124f6d305c9ee3e9e3eda9af4757fbe559408
Iliya Iliev <iliyailiev3592@gmail.com>
2023-05-07 19:37:46 +0300

    build: vm: fedora: Enable compression on disk
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

--- builds/vm/fedora/build_vm.sh
+++ builds/vm/fedora/build_vm.sh
@@ -1,9 +1,12 @@
-#!/bin/bash -e
+#!/bin/bash
+
+VM_DISK_IMAGE="./f37vm.qcow2"
 
 # Build Fedora 37 VM & install deps
-virt-builder fedora-37 -o f37vm.qcow2 --format qcow2 \
+virt-builder fedora-37 -o $VM_DISK_IMAGE --format qcow2 \
     --root-password password:fedora \
-    --update --selinux-relabel --size 40G \
+    --timezone "`cat /etc/timezone`" \
+    --update --selinux-relabel \
     --install "git,clang,llvm,m4,kernel-headers,cmake,bpftool,elfutils-libelf-devel,libpcap-devel,perf,pkg-config,python3,python3-pip" \
     --upload xdp_iface_setup.sh:/home/xdp_iface_setup.sh \
     --run-command "chmod +x /home/xdp_iface_setup.sh" \
@@ -12,3 +15,8 @@ virt-builder fedora-37 -o f37vm.qcow2 --format qcow2 \
     --run-command "sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config" \
     --append-line '/root/.bashrc:export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64/' \
     --append-line '/root/.bashrc:export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib'
+
+# Enable compression on disk
+mv "$VM_DISK_IMAGE" "$VM_DISK_IMAGE.old"
+qemu-img convert -O qcow2 -c "$VM_DISK_IMAGE.old" "$VM_DISK_IMAGE"
+rm -f "$VM_DISK_IMAGE.old"
