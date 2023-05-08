4220bad315f6c97440ee11c63a38ab1691079586
Iliya Iliev <iliyailiev3592@gmail.com>
2023-05-08 14:50:09 +0300

    builds: vm: fedora: Test py & go bindings on VM
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

--- builds/vm/fedora/xdp_iface_test.sh
+++ builds/vm/fedora/xdp_iface_test.sh
@@ -9,7 +9,10 @@ wait-for-it "$IP:22" -t 900 -s -- echo ready
 
 set -x
 sshpass -p toor ssh -o "StrictHostKeyChecking=no" "root@$IP" uname -a
+
 sshpass -p toor ssh "root@$IP" "/home/xdp_iface_work_dir/xdp_iface/build/xdpiface_selftest"
+sshpass -p toor ssh "root@$IP" "/usr/bin/python3 /home/xdp_iface_work_dir/xdp_iface/bindings/python/xdpiface_app.py"
+sshpass -p toor ssh "root@$IP" "cd /home/xdp_iface_work_dir/xdp_iface/bindings/go/xdpiface/ && go test -c && ./xdpiface.test"
 
 sudo virsh shutdown f37vm
 until sudo virsh domstate f37vm | grep shut; do
